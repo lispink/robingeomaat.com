9a07f754fbf69ba8340ccba0295b3cae2b2b635d
Robin <lispink000@gmail.com>
2019-03-19 00:10:54 +0100

    replicated nav bar

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+
+
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		TopNavComponent
 	],
 	imports: [
+		NgbCollapseModule,
+		NgbDropdownModule,
 		BrowserModule,
 		AppRoutingModule
 	],
